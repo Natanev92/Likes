4be39a8916adb65431d096145cb91e022b438c10
Natanev92 <nvillasenor1@icloud.com>
2021-05-25 16:24:09 -0700

    likes final

---
+++ script.js
@@ -0,0 +1,26 @@
+var count = 1;
+var countElement = document.querySelector("#count")
+console.log(countElement)
+function add1(){
+    count++;
+    countElement.innerText = count + "Like(s)" ;
+    console.log(count);
+}
+
+var count1 = 1;
+var countElement = document.querySelector("#count1")
+console.log(countElement)
+function add1(){
+    count1++;
+    count1Element.innerText = count1 + "Like(s)" ;
+    console.log(count1);
+}
+
+var count2 = 1;
+var countElement = document.querySelector("#count2")
+console.log(countElement)
+function add1(){
+    count2++;
+    countElement.innerText = count2 + "Like(s)" ;
+    console.log(count2);
+}
